8105b3b8734750da173502c79a1172a64448718f
Kirill <blayner0027@gmail.com>
2021-11-05 15:15:12 +0400

    Сделал ифраструктуру и написал контекст для бд

---
+++ Notes.Persistence/DbInitializer.cs
@@ -0,0 +1,8 @@
+﻿namespace Notes.Persistence
+{
+    public static class DbInitializer
+    {
+	    public static void Initialize(NotesDbContext context) =>
+		    context.Database.EnsureCreated();
+    }
+}
